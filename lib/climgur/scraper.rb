45582a966a4341e4d6d15e225e28e25e97d29fe4
brad72287 <brad72287@gmail.com>
2016-05-13 10:10:51 +0000

    adding filtering to display just images, not gifs and albums

--- lib/climgur/scraper.rb
+++ lib/climgur/scraper.rb
@@ -4,22 +4,34 @@ require 'pry'
 require 'asciiart'
 
 class Climgur
-  attr_accessor:
+  attr_accessor :images
 
-  def scrape-main-page
-    mainpage = Nokogiri::HTML(open("http://www.imgur.com"))
-    mainpage.css("div.post").each do |preview|
-    bar = {}
-    bar[:description] = preview.css("div.hover p").text
-    bar[:preview_url] = preview.css(".image-list-link img").first["src"]
-    foo << bar
-end
-    
-    #binding.pry
+  def initialize
+    @images = []
   end
 
+  def scrape_main_page
+    mainpage = Nokogiri::HTML(open("http://www.imgur.com"))
+    mainpage.css("div.post").each do |preview|
+      hash = {}
+      hash[:description] = preview.css("div.hover p").text
+      hash[:preview_url] = preview.css(".image-list-link img").first["src"]
+      hash[:type] = preview.css("div.hover div.post-info").text #because I want to filter out galleries and animated gifs
+      @images << hash
+    end
+    @images
+  end
+
+  def filter_images
+    self.images.select {|x| x[:type].include?('image')}
+  end
+
+
 end
 
+hello = Climgur.new
+hello.scrape_main_page
+puts hello.filter_images
 # Scraper.new.geturl.css("div.cards div.hover p").each {|x| puts x.text}  #preview description title
 # Scraper.new.geturl.css(".image-list-link img").each {|x| puts x['src']} #preview image url
 
