dd6167142b74c5b8a24117d44f94c7cdb8ca64a0
brad72287 <brad72287@gmail.com>
2016-05-13 06:51:21 +0000

    continuing to work on scraper

--- lib/climgur/scraper.rb
+++ lib/climgur/scraper.rb
@@ -1,15 +1,44 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
+require 'asciiart'
 
-class Scraper
+class Climgur
+  attr_accessor:
 
-  def geturl
-    file = Nokogiri::HTML(open("http://www.imgur.com"))
+  def scrape-main-page
+    mainpage = Nokogiri::HTML(open("http://www.imgur.com"))
+    mainpage.css("div.post").each do |preview|
+    bar = {}
+    bar[:description] = preview.css("div.hover p").text
+    bar[:preview_url] = preview.css(".image-list-link img").first["src"]
+    foo << bar
+end
+    
     #binding.pry
   end
 
 end
 
-Scraper.new.geturl.css("div.cards div.hover p").each {|x| puts x.text}
-Scraper.new.geturl.css(".image-list-link img").each {|x| puts x['src']}
+# Scraper.new.geturl.css("div.cards div.hover p").each {|x| puts x.text}  #preview description title
+# Scraper.new.geturl.css(".image-list-link img").each {|x| puts x['src']} #preview image url
+
+# foo = []
+# Scraper.new.geturl.css("div.post").each do |preview|
+#   bar = {}
+#   bar[:description] = preview.css("div.hover p").text
+#   bar[:preview_url] = preview.css(".image-list-link img").first["src"]
+#   foo << bar
+# end
+
+# puts foo[1][:preview_url]
+
+# x=0
+# while x < 5
+#   a = AsciiArt.new("http:"+foo[x][:preview_url])
+#   puts foo[x][:description]
+#   puts a.to_ascii_art(color: true, width: 30)
+#   x+=1
+# end
+
+
